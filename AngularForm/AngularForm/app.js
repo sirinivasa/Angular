ddcdd0452db66a68de0ab0aebfc172d59977da9b
Srinivasa <srinuvs2000rao@gmail.com>
2017-05-01 11:56:30 +0530

    Mytestangularjs

---
+++ AngularForm/AngularForm/app.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="F:\AnjularjsExamples\AngularForm\AngularForm\scripts/angular.js" />
+var app = angular.module('mainapp', []);
+
+app.controller('studentController', function ($scope) {
+    $scope.reset = function () {
+        $scope.firstName = "Mahesh";
+        $scope.lastName = "Parashar";
+        $scope.email = "MaheshParashar@tutorialspoint.com";
+    }
+    $scope.reset();
+
+})
